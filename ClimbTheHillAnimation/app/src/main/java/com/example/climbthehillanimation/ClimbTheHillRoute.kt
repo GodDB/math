4b3538f3bdb610eb9479fe9918fb1d90c03e8587
beom.93 <beom.93@kakaomobility.com>
2022-08-13 11:08:58 +0900

    canvas 생성

--- ClimbTheHillAnimation/app/src/main/java/com/example/climbthehillanimation/ClimbTheHillRoute.kt
+++ ClimbTheHillAnimation/app/src/main/java/com/example/climbthehillanimation/ClimbTheHillRoute.kt
@@ -1,9 +1,11 @@
 package com.example.climbthehillanimation
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.climbthehillanimation.ui.theme.ClimbTheHillAnimationTheme
 
@@ -17,7 +19,17 @@ fun ClimbTheHillScreen() {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
+        ClimbTheHillRenderer(modifier = Modifier.fillMaxSize())
 
+    }
+}
+
+@Composable
+private fun ClimbTheHillRenderer(
+    modifier: Modifier
+) {
+    Canvas(modifier = modifier) {
+        //TODO
     }
 }
 
