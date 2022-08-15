0bb357645f1637c7aaa87bfbc4dbbb55d28804ed
beom.93 <beom.93@kakaomobility.com>
2022-08-15 11:56:25 +0900

    베지어 2차 곡선 적용

--- ClimbTheHillAnimation/app/src/main/java/com/example/climbthehillanimation/ClimbTheHillRoute.kt
+++ ClimbTheHillAnimation/app/src/main/java/com/example/climbthehillanimation/ClimbTheHillRoute.kt
@@ -1,35 +1,80 @@
 package com.example.climbthehillanimation
 
+import android.graphics.Point
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.climbthehillanimation.ui.theme.ClimbTheHillAnimationTheme
 
 @Composable
-fun ClimbTheHillRoute(viewModel : ClimbTheHillViewModel) {
-    ClimbTheHillScreen()
+fun ClimbTheHillRoute(viewModel: ClimbTheHillViewModel) {
+    val state by viewModel.climbTheHillState.collectAsState()
+    ClimbTheHillScreen(
+        state = state
+    )
 }
 
 @Composable
-fun ClimbTheHillScreen() {
+fun ClimbTheHillScreen(
+    modifier: Modifier = Modifier,
+    state: ClimbTheHillState
+) {
     Surface(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .then(modifier)
     ) {
-        ClimbTheHillRenderer(modifier = Modifier.fillMaxSize())
-
+        ClimbTheHillRenderer(
+            modifier = Modifier.fillMaxSize(),
+            state = state
+        )
     }
 }
 
 @Composable
 private fun ClimbTheHillRenderer(
-    modifier: Modifier
+    modifier: Modifier,
+    state: ClimbTheHillState
 ) {
     Canvas(modifier = modifier) {
-        //TODO
+        drawHills(state.hills, Paint().apply {
+            isAntiAlias = true
+            strokeWidth = 10f
+            style = PaintingStyle.Stroke
+            color = Color.Black
+        })
+    }
+}
+
+
+private fun DrawScope.drawHills(hills: List<Hill>, paint: Paint) {
+    val path = Path()
+    hills.forEach { hill ->
+        var prevPoint = hill.points[0]
+        drawRect(Color.Green, Offset(prevPoint.x.toFloat(), prevPoint.y.toFloat()), size = Size(50f, 50f))
+        for (i in 1 until hill.points.size) {
+            val curPoint = hill.points[i]
+            val midPoint = Point((prevPoint.x + curPoint.x) / 2, (prevPoint.y + curPoint.y) / 2)
+            path.quadraticBezierTo(prevPoint.x.toFloat(), prevPoint.y.toFloat(), midPoint.x.toFloat(), midPoint.y.toFloat())
+            this.drawContext.canvas.drawPath(path, paint)
+            drawRect(Color.Green, Offset(curPoint.x.toFloat(), curPoint.y.toFloat()), size = Size(50f, 50f)) //원래 포인트 그린
+            drawRect(Color.Blue, Offset(midPoint.x.toFloat(), midPoint.y.toFloat()), size = Size(50f, 50f)) // 보간 처리된 블루
+            prevPoint = curPoint
+        }
+
     }
 }
 
@@ -38,7 +83,7 @@ private fun ClimbTheHillRenderer(
 @Composable
 fun PreviewClimbTheHillScreen() {
     ClimbTheHillAnimationTheme {
-        ClimbTheHillScreen()
+        /*   ClimbTheHillScreen()*/
     }
 }
 
--- ClimbTheHillAnimation/app/src/main/java/com/example/climbthehillanimation/ClimbTheHillViewModel.kt
+++ ClimbTheHillAnimation/app/src/main/java/com/example/climbthehillanimation/ClimbTheHillViewModel.kt
@@ -1,6 +1,92 @@
 package com.example.climbthehillanimation
 
+import android.graphics.Point
+import android.util.Log
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class ClimbTheHillViewModel : ViewModel() {
+
+    private val hillFlow: MutableStateFlow<List<Hill>> = MutableStateFlow(listOf())
+    private val climberFlow: MutableStateFlow<List<Climber>> = MutableStateFlow(listOf())
+
+    val climbTheHillState: StateFlow<ClimbTheHillState> = combine(hillFlow, climberFlow) { hills, climbers ->
+        ClimbTheHillState(hills, climbers)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000L),
+        initialValue = ClimbTheHillState(listOf(), listOf())
+    )
+
+    init {
+        viewModelScope.launch {
+            val hills = List(1) {
+                Hill.create(MAX_HEIGHT, 200)
+            }
+            Log.e("godgod", "$hills")
+            hillFlow.emit(hills)
+        }
+
+    }
+
+
+    private suspend fun startRender() {
+        viewModelScope.launch {
+
+            startRender()
+        }
+    }
+
+    private suspend fun endRender() {
+
+    }
+
+    companion object {
+        private const val FPS = 30 // 초당 30프레임
+        private const val MAX_HEIGHT = 2000
+    }
 }
+
+data class ClimbTheHillState(
+    val hills: List<Hill>,
+    val climbers: List<Climber>
+)
+
+data class Hill(
+    val points : List<Point>,
+    val speed: Int
+) {
+
+    companion object {
+        private const val INTERVAL_X = 200
+
+        fun create(maxHeight: Int, speed : Int = 200): Hill {
+            val heightRange = (0 .. maxHeight)
+            return Hill(
+                points = List(size = 10) { index ->
+                    val x = INTERVAL_X * index
+                    Point(x, heightRange.random())
+                },
+                speed = speed
+            )
+        }
+    }
+}
+
+data class Climber(
+    val x: Int,
+    val y: Int,
+    val targetHill: Hill,
+    val speed: Int
+)
+
+
